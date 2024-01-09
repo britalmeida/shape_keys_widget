14fd61d6e52d41bdc692048ed058ad1b6aabd2d3
Inês Almeida <britalmeida@gmail.com>
2024-01-09 11:22:42 +0100

    Test: using panel button to key selected category shape key values

--- src_skw/ops.py
+++ src_skw/ops.py
@@ -11,7 +11,7 @@ from bpy.types import Operator
 class ANIM_OT_insert_keyframes_shapekey_category(Operator):
     bl_idname = "anim.skw_insert_keyframes_shapekey_category"
     bl_label = "Keyframe Category"
-    bl_description = "Add keyframes for the Shape Keys of the (selected?) category"
+    bl_description = "Add keyframes for the Shape Keys of the category with the selected cursor"
     bl_options = {'UNDO', 'REGISTER'}
 
     @classmethod
@@ -22,7 +22,15 @@ class ANIM_OT_insert_keyframes_shapekey_category(Operator):
         """Called to finish this operator's action.
         """
 
-        print("Added a keyframe for all Shape Keys in a category")
+        for selected_object in bpy.context.selected_objects:
+            if "-cursor" in selected_object.name:
+                category = "Eyes"
+                mesh_obj = bpy.data.objects["GEO-face"]
+                for key in mesh_obj.data.shape_keys.key_blocks:
+                    if key.name.startswith("Eyes - "):
+                        if not key.name.endswith("- Neutral"):
+                            current_frame = context.scene.frame_current
+                            key.keyframe_insert(data_path="value", frame=current_frame)
 
         return {'FINISHED'}
 
