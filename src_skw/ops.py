0d066e2a72f12a663f2b27fae721d17418e30ceb
Inês Almeida <britalmeida@gmail.com>
2024-01-12 12:33:43 +0100

    Remove tests for UI and mechanisms for keyframing

--- src_skw/ops.py
+++ src_skw/ops.py
@@ -5,105 +5,12 @@ import logging
 log = logging.getLogger(__package__)
 
 import bpy
-from bpy.props import BoolProperty
-from bpy.types import Operator
-
-def keyframe_sk_influence(cursor_obj, frame):
-    name_parts = cursor_obj.name.split('-')
-    if name_parts[1] != "skw":
-        # Object does not have the naming convention of a Shape Key Widget Cursor.
-        return
-
-    mesh_col_name = name_parts[0]+"-model"
-    category_name = name_parts[2].capitalize()+" - "
-
-    mesh_obj = bpy.data.collections[mesh_col_name].objects[0]
-    mesh_data = mesh_obj.data
-
-    for key in mesh_data.shape_keys.key_blocks:
-        if key.name.startswith(category_name) and not key.name.endswith("- Neutral"):
-            key.keyframe_insert(data_path="value", frame=frame)
-
-
-def keyframe_skw_cursor_pos(cursor_obj, frame):
-    name_parts = cursor_obj.name.split('-')
-    if name_parts[1] != "skw":
-        # Object does not have the naming convention of a Shape Key Widget Cursor.
-        return
-
-    cursor_obj.keyframe_insert(data_path="location", frame=frame)
-
-
-class ANIM_OT_insert_keyframes_shapekey_category(Operator):
-    bl_idname = "anim.skw_insert_keyframes_shapekey_category"
-    bl_label = "Keyframe Category"
-    bl_description = "Add keyframes for the Shape Keys of the category with the selected cursor"
-    bl_options = {'UNDO', 'REGISTER'}
-
-    keyframe_values: BoolProperty(
-        name="Keyframe SK Values",
-        default=True,
-    )
-    keyframe_cursor: BoolProperty(
-        name="Keyframe Cursor",
-        default=True,
-    )
-
-    @classmethod
-    def poll(cls, context):
-        return True
-
-    def execute(self, context):
-        current_frame = context.scene.frame_current
-
-        for selected_object in context.selected_objects:
-            if selected_object.name.endswith("-cursor"):
-                if self.keyframe_cursor:
-                    keyframe_skw_cursor_pos(selected_object, current_frame)
-                if self.keyframe_values:
-                    keyframe_sk_influence(selected_object, current_frame)
-
-        return {'FINISHED'}
-
-
-class ANIM_OT_insert_keyframes_all_shapekeys(Operator):
-    bl_idname = "anim.skw_insert_keyframes_all_categories"
-    bl_label = "Keyframe All"
-    bl_description = "Add keyframes for all Shape Keys in all widget categories for all characters"
-    bl_options = {'UNDO', 'REGISTER'}
-
-    keyframe_values: BoolProperty(
-        name="Keyframe SK Values",
-        default=True,
-    )
-    keyframe_cursor: BoolProperty(
-        name="Keyframe Cursor",
-        default=True,
-    )
-
-    @classmethod
-    def poll(cls, context):
-        return True
-
-    def execute(self, context):
-        current_frame = context.scene.frame_current
-
-        for collection in bpy.data.collections:
-            if collection.name.endswith("-cursor"):
-                if self.keyframe_cursor:
-                    keyframe_skw_cursor_pos(collection, current_frame)
-                if self.keyframe_values:
-                    keyframe_sk_influence(collection, current_frame)
-
-        return {'FINISHED'}
 
 
 
 # Add-on Registration #############################################################################
 
 classes = (
-    ANIM_OT_insert_keyframes_shapekey_category,
-    ANIM_OT_insert_keyframes_all_shapekeys,
 )
 
 
--- src_skw/ui.py
+++ src_skw/ui.py
@@ -8,101 +8,26 @@ import bpy
 from bpy.types import Panel
 
 
-class VIEW3D_PT_anim_sel_cursor(Panel):
+class VIEW3D_PT_shape_key_widgets(Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "Shape Key Widgets"
-    bl_label = "Animation (SKS)"
-    bl_description = """As in SKS, key selected cursor category.
-    Key Shape Key values and cursor location"""
+    bl_label = "WIP"
+    bl_description = """Configuration of thumbnails and categories should go here."""
 
     def draw(self, context):
         layout = self.layout
         layout.use_property_split = True
 
         col = layout.column()
-        col.operator("anim.skw_insert_keyframes_shapekey_category")
-        col.operator("anim.skw_insert_keyframes_all_categories")
-
-
-class VIEW3D_PT_anim_listed_values(Panel):
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Shape Key Widgets"
-    bl_label = "Animation (SK values)"
-    bl_description = """Selection doesn't matter.
-    Key Shape Key values, but *not* SKWidget cursor location"""
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-
-        # Collect all the existing SKW cursors per mesh.
-        meshes = {}
-        for collection in bpy.data.collections:
-            if collection.name.endswith("-skw"):
-                mesh_col_name = collection.name[:-4]
-                meshes[mesh_col_name] = []
-
-                for obj in collection.objects:
-                    name_parts = obj.name.split('-')
-                    if len(name_parts) == 3:
-                        category_name = name_parts[2]
-                        meshes[mesh_col_name].append(category_name)
-
-        for mesh_col_name in sorted(meshes):
-            col = layout.column(align=True)
-            col.label(text=mesh_col_name.capitalize()+":")
-            row = col.row(align=True)
-            for category_name in sorted(meshes[mesh_col_name]):
-                row.operator("anim.skw_insert_keyframes_shapekey_category",
-                    text=category_name.capitalize()).keyframe_cursor = False
-            col.operator("anim.skw_insert_keyframes_shapekey_category",
-                    text="All").keyframe_cursor = False
-
-
-class VIEW3D_PT_anim_listed_cursor(Panel):
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Shape Key Widgets"
-    bl_label = "Animation (cursor)"
-    bl_description = """Key the SKWidget cursor location, but *not* Shape Key values"""
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-
-        # Collect all the existing SKW cursors per mesh.
-        meshes = {}
-        for collection in bpy.data.collections:
-            if collection.name.endswith("-skw"):
-                mesh_col_name = collection.name[:-4]
-                meshes[mesh_col_name] = []
-
-                for obj in collection.objects:
-                    name_parts = obj.name.split('-')
-                    if len(name_parts) == 3:
-                        category_name = name_parts[2]
-                        meshes[mesh_col_name].append(category_name)
-
-        for mesh_col_name in sorted(meshes):
-            col = layout.column(align=True)
-            col.label(text=mesh_col_name.capitalize()+":")
-            row = col.row(align=True)
-            for category_name in sorted(meshes[mesh_col_name]):
-                row.operator("anim.skw_insert_keyframes_shapekey_category",
-                    text=category_name.capitalize()).keyframe_values = False
-            col.operator("anim.skw_insert_keyframes_shapekey_category",
-                    text="All").keyframe_values = False
+        col.label(text="Hello again :)")
 
 
 
 # Add-on Registration #############################################################################
 
 classes = (
-    VIEW3D_PT_anim_sel_cursor,
-    VIEW3D_PT_anim_listed_values,
-    VIEW3D_PT_anim_listed_cursor,
+    VIEW3D_PT_shape_key_widgets,
 )
 
 
