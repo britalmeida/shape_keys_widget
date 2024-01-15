d8e5c252f0ee596ab16851dbfe907b90d7f2b81b
Inês Almeida <britalmeida@gmail.com>
2024-01-15 21:53:48 +0100

    SKS conversion: automate setting the character name

--- src_skw/ui.py
+++ src_skw/ui.py
@@ -5,29 +5,59 @@ import logging
 log = logging.getLogger(__package__)
 
 import bpy
-from bpy.types import Panel
+from bpy.props import StringProperty
+from bpy.types import AddonPreferences, Panel
 
 
 class VIEW3D_PT_shape_key_widgets(Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "Item" #"Shape Key Widgets"
-    bl_label = "WIP"
+    bl_label = "Setup Shape Key Widgets"
     bl_description = """Configuration of thumbnails and categories should go here."""
+
+
 
     def draw(self, context):
         layout = self.layout
         layout.use_property_split = True
 
-        col = layout.column()
-        col.operator("scene.convert_sks_to_skw")
+        addon_prefs = context.preferences.addons[__package__.split('.')[0]].preferences
+        char_name = addon_prefs.character_name
 
+        col = layout.column()
+        col.prop(addon_prefs, "character_name")
+        col.prop(addon_prefs, "categories_str")
+
+        op = col.operator("scene.convert_sks_to_skw")
+        op.rig_name = "RIG-"+char_name
+        op.geo_name = f"GEO-{char_name}-head"
+        op.thumbs_collection_name = f"{char_name}-rig-widgets-thumbnails"
+        op.wgts_collection_name = f"{char_name}-rig-widgets"
+        op.categories_str = addon_prefs.categories_str
+
+
+class VIEW3D_ShapeKeyWidgets_Preferences(AddonPreferences):
+    bl_idname = __package__.split('.')[0]
+
+    character_name: StringProperty(
+        name="Character Name",
+        description="Existing rig where to add new bone controls for the shape keys",
+        default="claudia",
+    )
+
+    categories_str: StringProperty(
+        name="Shape Key Categories",
+        description="Comma separated list of Shape Key categories to generate controls for",
+        default="Mouth, Eyes",
+    )
 
 
 # Add-on Registration #############################################################################
 
 classes = (
     VIEW3D_PT_shape_key_widgets,
+    VIEW3D_ShapeKeyWidgets_Preferences,
 )
 
 
